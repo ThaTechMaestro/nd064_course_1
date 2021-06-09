ee69df1a8ee0fd5e944587d574d38756d12e1138
ThaTechMaestro <muse.pearls@gmail.com>
2021-06-09 22:13:23 +0100

    My InstructorSoln: Lesson2 num 11

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, json
+import logging
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,14 +8,24 @@ def hello():
     return "Hello World!"
 
 @app.route("/status")
-def status_response():
-    status = {'result':'OK - healthy'}
-    return jsonify(status)
+def health_check():
+    response = app.response_class(
+        response=json.dumps({"result":"Ok - healthy"}),
+        status = 200,
+        mimetype='application/json'
+    )
+
+    return response
 
 @app.route("/metrics")
 def metrics():
-    metrics = {'UserCount':'40', 'UserCountActive':'23'}
-    return jsonify(metrics)
+    response = app.response_class(
+        response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140,"UserCountActive":23}}),
+        status = 200,
+        mimetype='application/json'
+    )
+
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
