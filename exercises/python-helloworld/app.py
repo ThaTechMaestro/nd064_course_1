f62150dbe622a862190fef7617df54445bd05ffb
ThaTechMaestro <muse.pearls@gmail.com>
2021-06-09 21:31:37 +0100

    My Solution: Lesson2 num 11

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/status")
+def status_response():
+    status = {'result':'OK - healthy'}
+    return jsonify(status)
+
+@app.route("/metrics")
+def metrics():
+    metrics = {'UserCount':'40', 'UserCountActive':'23'}
+    return jsonify(metrics)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
